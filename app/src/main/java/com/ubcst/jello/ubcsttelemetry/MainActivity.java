8b3004a6edb0aa7a402ae69e8cd6d1b18f396f9d
Jan Louis Evangelista <jlevangelista@alumni.ubc.ca>
2016-03-19 13:50:43 -0700

    Add Connect Device button to initialize the USB

--- app/src/main/java/com/ubcst/jello/ubcsttelemetry/MainActivity.java
+++ app/src/main/java/com/ubcst/jello/ubcsttelemetry/MainActivity.java
@@ -1,25 +1,48 @@
 package com.ubcst.jello.ubcsttelemetry;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.usb.UsbManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 import com.ubcst.jello.ubcsttelemetry.http.VolleyRequests;
 
 public class MainActivity extends AppCompatActivity {
+
+    Intent intent = new Intent();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        UsbPhone.manager = (UsbManager)getSystemService(Context.USB_SERVICE);
+        UsbPhone.linuxPC = intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
+
+
+
         // TODO: Move somewhere else. Just placing here for testing.
         // TODO: Check what context to pass into the the constructor.
         // Commenting out for now.
         /* VolleyRequests volleyRequests = new VolleyRequests(this.getApplicationContext());
         volleyRequests.mainLoop(); */
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        UsbPhone.closeAccessory();
+        unregisterReceiver(UsbPhone.mUsbReceiver);
+
+        super.onDestroy();
     }
 
     public void gotoGPS(View view)
@@ -32,5 +55,11 @@ public class MainActivity extends AppCompatActivity {
     {
         Intent intent = new Intent(this, sensorActivity.class);
         startActivity(intent);
+    }
+
+    public void gotoConnect(View view)
+    {
+        // Connect to the USB device
+        UsbPhone.openAccessory();
     }
 }
--- app/src/main/java/com/ubcst/jello/ubcsttelemetry/gpsActivity.java
+++ app/src/main/java/com/ubcst/jello/ubcsttelemetry/gpsActivity.java
@@ -1,30 +1,15 @@
 package com.ubcst.jello.ubcsttelemetry;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.hardware.usb.UsbAccessory;
-import android.hardware.usb.UsbManager;
-import android.os.ParcelFileDescriptor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import java.io.FileDescriptor;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
 
-public class gpsActivity extends AppCompatActivity implements Runnable {
+public class gpsActivity extends AppCompatActivity{
 
-    Intent intent;
-
-    private UsbManager manager;
-    private UsbAccessory linuxPC;
-    private ParcelFileDescriptor mFileDescriptor;
-    private FileInputStream mInputStream;
-    private FileOutputStream mOutputStream;
+    Intent intent = getIntent();
 
     private double latitude;
     private double longitude;
@@ -38,20 +23,16 @@ public class gpsActivity extends AppCompatActivity implements Runnable {
     TextView timeMsg;
     TextView rawMsg;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        intent = getIntent();
-        manager = (UsbManager)getSystemService(Context.USB_SERVICE);
-        linuxPC = intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
+        setContentView(R.layout.activity_gps);
 
         latitudeMsg = (TextView) findViewById(R.id.latitudeField);
         longitudeMsg = (TextView) findViewById(R.id.longitudeField);
         timeMsg = (TextView) findViewById(R.id.timeField);
         rawMsg = (TextView) findViewById(R.id.rawDataField);
-
-        setContentView(R.layout.activity_gps);
     }
 
     @Override
@@ -59,89 +40,16 @@ public class gpsActivity extends AppCompatActivity implements Runnable {
     {
         super.onResume();
 
-        openAccessory();
+        rawMsg.setText(UsbPhone.getString());
     }
 
     @Override
     public void onDestroy()
     {
-        closeAccessory();
-        unregisterReceiver(mUsbReceiver);
         super.onDestroy();
+
+        // TODO Add null check before closing
+        UsbPhone.closeAccessory();
+        unregisterReceiver(UsbPhone.mUsbReceiver);
     }
-
-    private void openAccessory()
-    {
-        mFileDescriptor = manager.openAccessory(linuxPC);
-        if(mFileDescriptor != null)
-        {
-            FileDescriptor fd = mFileDescriptor.getFileDescriptor();
-            mInputStream = new FileInputStream(fd);
-            mOutputStream = new FileOutputStream(fd);
-            Thread thread = new Thread(null, this, "AccessoryThread");
-            thread.start();
-        }
-    }
-
-    private void closeAccessory()
-    {
-        try
-        {
-            if(mFileDescriptor != null)
-            {
-                mFileDescriptor.close();
-            }
-        }
-        catch (IOException e)
-        {
-        }
-        finally
-        {
-            mFileDescriptor = null;
-            linuxPC = null;
-        }
-    }
-
-    BroadcastReceiver mUsbReceiver = new BroadcastReceiver()
-    {
-        public void onReceive(Context context, Intent intent)
-        {
-            String action = intent.getAction();
-
-            if (UsbManager.ACTION_USB_ACCESSORY_DETACHED.equals(action))
-            {
-                UsbAccessory accessory = intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
-                if (accessory != null) {
-                    closeAccessory();
-                }
-            }
-        }
-    };
-
-    @Override
-    public void run()
-    {
-        int response = 0;
-        byte[] message = new byte[16384];
-
-        try
-        {
-            response = mInputStream.read(message);
-        }
-        catch( IOException e)
-        {
-            // Exception
-        }
-
-        if(response < 0)
-        {
-            rawMsg.setText("ERROR READING STREAM");
-        }
-        else
-        {
-            String str = new String(message);
-            rawMsg.setText(str);
-        }
-    }
-
 }
